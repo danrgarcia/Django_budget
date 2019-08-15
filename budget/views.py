11a6c3eac4faed9fb757355aa6f2440b69842a1d
Daniel <danielrichardgarcia2@gmail.com>
2019-08-15 10:52:15 -0700

    Add files via upload

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Category
+from django.views.generic import CreateView
 
 # Create your views here.
 def project_list(request):
@@ -7,4 +8,26 @@ def project_list(request):
 
 
 def project_detail(request, project_slug):
-    return render(request, 'budget/project_detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project_detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories.self.request.POST['categoriesString'].split(',')
+        for category in categories:
+            Category.objects.create(
+                project = Project.objects.get(id=self.object.id)
+                name = category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
